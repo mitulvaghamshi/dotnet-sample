4dc963887f9a6da46020d5dec1c9bcddd18f81b7
mitulvaghamshi <mitulvaghmashi@gmail.com>
2023-04-01 19:24:50 -0400

    Add example Database Migrations; Update README.md; Fix walkthrough 8.md

---
+++ examples/MovieTracker-p5/MovieTracker/Migrations/20230401205837_TitleLength.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieTracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class TitleLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movie",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.UpdateData(
+                table: "Movie",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateSeen",
+                value: new DateTime(2023, 2, 10, 16, 58, 37, 418, DateTimeKind.Local).AddTicks(2513));
+
+            migrationBuilder.UpdateData(
+                table: "Movie",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "DateSeen",
+                value: new DateTime(2023, 3, 7, 16, 58, 37, 418, DateTimeKind.Local).AddTicks(2547));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.UpdateData(
+                table: "Movie",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateSeen",
+                value: new DateTime(2023, 2, 10, 16, 45, 49, 576, DateTimeKind.Local).AddTicks(3653));
+
+            migrationBuilder.UpdateData(
+                table: "Movie",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "DateSeen",
+                value: new DateTime(2023, 3, 7, 16, 45, 49, 576, DateTimeKind.Local).AddTicks(3685));
+        }
+    }
+}
---
+++ examples/MovieTracker-p5/MovieTracker/Models/Movie.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MovieTracker.Models
+{
+	public class Movie
+	{
+		[Key]
+		public int Id { get; set; }
+
+		[Required]
+		[MaxLength(100)]
+		public string? Title { get; set; }
+
+		[DisplayName("Date Seen")]
+		[DataType(DataType.Date)]
+		[DateSeenValidation(ErrorMessage = "Date Seen cannot be a future date.")]
+		public DateTime? DateSeen { get; set; }
+
+		// Navigation property
+		[ForeignKey(nameof(Genre))]
+		[Display(Name = nameof(Genre))]
+		public int? GenreId { get; set; }
+
+		// Navigation property
+		public Genre? Genre { get; set; }
+
+		[Range(1, 10)]
+		public int? Rating { get; set; }
+
+		[Display(Name = "Release Year")]
+		public int? ReleaseYear { get; set; }
+	}
+}
