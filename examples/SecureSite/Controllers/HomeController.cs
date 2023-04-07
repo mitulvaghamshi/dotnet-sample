b92d12811167a3a822ff7af4975b269fe0778d61
mitulvaghamshi <mitulvaghmashi@gmail.com>
2023-04-06 20:01:56 -0400

    Add Auth (Identity) example; Update README.md; Fix walkthrough 15 and Page 13

---
+++ examples/SecureSite/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SecureSite.Models;
+using System.Diagnostics;
+
+namespace SecureSite.Controllers;
+
+[Authorize]
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+
+    public HomeController(ILogger<HomeController> logger) => _logger = logger;
+
+    [AllowAnonymous]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy() => View();
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error() => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+}
