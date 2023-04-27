a813580b3bfdf028aa865139ea9fdcd9c4138c19
mitulvaghamshi <mitulvaghmashi@gmail.com>
2023-04-27 00:26:09 -0400

    Add ColorPicker example; Update README.md

---
+++ examples/Colors/Data/ColorContext.cs
@@ -0,0 +1,20 @@
+﻿using ColorPicker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ColorPicker.Data;
+
+public partial class ColorContext : DbContext
+{
+    public ColorContext(DbContextOptions options) : base(options) { }
+
+    public virtual DbSet<Color> Color { get; set; }
+
+    public virtual DbSet<Group> Group { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlite("name=Color");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) => OnModelCreatingPartial(modelBuilder);
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
