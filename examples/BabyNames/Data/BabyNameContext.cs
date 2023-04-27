2e436c75eaf6ea1d65d723161e1de3a52068163d
mitulvaghamshi <mitulvaghmashi@gmail.com>
2023-04-27 00:29:27 -0400

    Add BabyNames example; Update README.md

---
+++ examples/BabyNames/Data/BabyNameContext.cs
@@ -0,0 +1,23 @@
+﻿using BabyNames.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BabyNames.Data;
+
+public partial class BabyNameContext : DbContext
+{
+    public BabyNameContext(DbContextOptions<BabyNameContext> options) : base(options) { }
+
+    public virtual DbSet<Religion> Religion { get; set; }
+
+    public virtual DbSet<BabyName> BabyName { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlite("name=BabyName");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ examples/BabyNames/Models/Religion.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BabyNames.Models;
+
+public partial class Religion
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required, StringLength(10)]
+    public string Name { get; set; } = string.Empty;
+
+    [InverseProperty(nameof(Religion))]
+    public virtual ICollection<BabyName> BabyNames { get; set; } = new List<BabyName>();
+}
